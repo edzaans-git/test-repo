285611908adf63c57c8066ede577ef85ff053ef9
Edgars Becs <g00377012@gmit.ie>
2020-10-17 20:01:03 +0100

    Second entry

--- ie/gmit/testpackage/TestClass.java
+++ ie/gmit/testpackage/TestClass.java
@@ -2,6 +2,6 @@ package ie.gmit.testpackage;
 
 public class TestClass {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello there for second time");
 	}
 }
